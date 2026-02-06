86184eb056c83b453af768584971dc3756817dec
tytus <tytus@baranscy.info>
2026-02-06 17:24:22 +0100

    ...

---
+++ src/Server/ChessEngine/Constants/GameLogic.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpChess.Constants
+{
+    internal class GameLogic
+    {
+    }
+}
---
+++ src/Server/ChessEngine/Pieces/Piece.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpChess
+{
+    public enum Team { white, black }
+
+    internal abstract class Piece
+    {
+        protected Piece(BoardSquare position, Team team)
+        {
+            _position = position;
+            Team = team;
+        }
+
+        private BoardSquare _position;
+        public BoardSquare Position { get { return _position; } }
+
+        public readonly Team Team;
+
+        abstract public bool DoMove(); // Attempt to move somewhere
+        abstract public List<BoardSquare> LegalMoves { get; } // list of moves the piece can do
+    }
+}
