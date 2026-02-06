067b5ce47f8a5d20559b0a6e1b22d16c637e9285
tytus <tytus@baranscy.info>
2026-02-06 17:23:57 +0100

    Add other services

---
+++ src/Server/ChessEngine/Board/ChessBoard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpChess
+{
+    internal static class ChessBoard
+    {
+        public static List<Piece> Pieces = new List<Piece> { };
+
+        public static bool IsTileOccupied(BoardSquare Tile)
+        {
+            foreach (var item in Pieces)
+            {
+                if (item.Position.Equals(Tile))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
