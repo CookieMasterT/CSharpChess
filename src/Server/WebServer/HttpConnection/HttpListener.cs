c68d82875f9f637c1d079c273d30a1c85a2ede57
tytus <tytus@baranscy.info>
2026-02-14 16:50:40 +0100

    Basic client with communication

---
+++ src/Server/WebServer/HttpConnection/HttpListener.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+
+namespace WebServer.HttpConnection
+{
+    public static class HttpConnection
+    {
+        public static async Task StartConnection(string port)
+        {
+            var listener = new HttpListener();
+            listener.Prefixes.Add(port);
+            listener.Start();
+
+            Console.WriteLine($"Listening on {port}");
+
+            while (true)
+            {
+                var context = await listener.GetContextAsync();
+                var response = context.Response;
+
+                response.Headers.Add("Access-Control-Allow-Origin", "*");
+                response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+                response.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
+
+                if (context.Request.HttpMethod == "OPTIONS")
+                {
+                    response.StatusCode = 200;
+                    response.Close();
+                    continue;
+                }
+
+                string requestBody = "";
+
+                if (context.Request.HasEntityBody)
+                {
+                    using (var reader = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
+                    {
+                        requestBody = await reader.ReadToEndAsync();
+                    }
+                }
+
+                // ✅ Pass body to HandleClient
+                string Response = await Program.HandleClient(requestBody);
+                byte[] buffer = Encoding.UTF8.GetBytes(Response);
+
+                response.ContentType = "application/json";
+                response.ContentLength64 = buffer.Length;
+                await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+
+                response.Close();
+            }
+        }
+    }
+}
---
+++ src/Server/WebServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using CSharpChess;
+
+namespace WebServer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await HttpConnection.HttpConnection.StartConnection("http://localhost:54321/");
+        }
+        public static async Task<string> HandleClient(string request)
+        {
+            Console.Write(request);
+            string json = "{\"message\":\"Hello from C#\"}";
+            return json;
+        }
+    }
+}
