d184cd7cdb8daf31e4072e2f9144a920c0ecb5d7
tytus <tytus@baranscy.info>
2026-02-18 16:56:51 +0100

    Add board displaying

--- src/Client/ChessGUI/js/app.js
+++ src/Client/ChessGUI/js/app.js
@@ -19,7 +19,7 @@ async function RequestInfo(type) {
       throw new Error(`Server responded with status: ${response.status}`);
     }
 
-    return await response.text();
+    return await response.json();
   }
 }
 
@@ -27,9 +27,54 @@ async function Init() {
   await InitChessBoard();
 }
 
+const FileNames = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+const RankNames = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
+const ImageFileNameDict = {"K": "king", "Q": "queen", "R": "rook", "B": "bishop", "N": "knight", "P":"pawn"}
+
 async function InitChessBoard() {
   let request = await RequestInfo("boardState")
-  console.log(request);
+  let board = document.getElementById("ChessBoard")
+  let html = "";
+
+  html += `<table>`
+  for (let i = 0; i < 10; i++) {
+    html += `<tr>`
+    for (let k = 0; k < 10; k++) {
+      if (k !== 0 && i !== 0 && i !== 9 && k !== 9) {
+        let x = k - 1;
+        let y = i - 1;
+        html += `<td class=\"tile-${(x % 2 + y % 2) % 2 === 1 ? "w" : "b"}">`
+
+        let tile = request[x][y]
+        if (tile !== "U") {
+          let color = tile[0]
+          let type;
+          if (tile.length === 1)
+            type = "P"
+          else
+            type = tile[1]
+
+          html += `<img id="piece-${x};${y}" src="img/${ImageFileNameDict[type]}-${color}.svg" alt="${tile}"/>`;
+        }
+        html += `</td>`
+      } else {
+        html += `<td class="tile-edge">`
+        if (i !== 0 && i !== 9) {
+          html += `<span>${RankNames[i - 1]}</span>`
+        } else {
+          if (k !== 0 && k !== 9) {
+            html += `<span>${FileNames[k - 1]}</span>`
+          }
+        }
+        html += `</td>`
+      }
+    }
+    html += `</tr>`
+  }
+  html += `</table>`
+
+  board.innerHTML = html;
 }
 
 Init().then();
--- src/Server/WebServer/GameDataParsers/ChessBoard.cs
+++ src/Server/WebServer/GameDataParsers/ChessBoard.cs
@@ -11,7 +11,7 @@ namespace WebServer.GameDataParsers
         public static string json { 
             get 
             {
-                var json = new JObject();
+                var json = new JArray();
                 for (int x = 0; x < 8; x++)
                 {
                     // todo: ensure that this is actually a rank
@@ -20,11 +20,19 @@ namespace WebServer.GameDataParsers
                     {
                         var str = string.Empty;
                         var tile = CSharpChess.ChessBoard.Board[x, y];
-                        str += tile.content?.Team;
+                        switch (tile.content?.Team)
+                        {
+                            case CSharpChess.Team.white:
+                                str += ChessNotation.WhiteTeam;
+                                break;
+                            case CSharpChess.Team.black:
+                                str += ChessNotation.BlackTeam;
+                                break;
+                        }
                         str += (tile.content?.Name ?? ChessNotation.EmptySquare);
                         rank.Add(str);
                     }
-                    json.Add(ChessNotation.LegalRankNames[x].ToString(), rank);
+                    json.Add(rank);
                 }
                 return json.ToString();
             } 
